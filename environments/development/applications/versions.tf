3f100d80f80544cba8cb7896fe672122e83904eb
e-d-o-c <79098019+e-d-o-c@users.noreply.github.com>
2023-06-05 17:20:11 +0100

    Core setup (#2)
    * dynamodb setup
    
    * dynamo setup
    
    * fix region
    
    * fix region

---
+++ environments/development/applications/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.2.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
