8c03290eec30e3d2214e90b7d911db2553c20343
Bun Camara <bun.camara@transformuk.com>
2023-06-07 15:49:37 +0100

    rs output values

---
+++ environments/development/applications/remote-state-output.tf
@@ -0,0 +1,17 @@
+
+/*
+ *   The terraform_remote_state data source allows us to
+ *   retrieve the root module output values from the base directory
+ */
+
+
+
+data "terraform_remote_state" "base" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-state-${var.environment}-${aws_account_id}"
+    key    = "base/terraform.tfstate"
+    region = var.region
+  }
+}
