71bf2c99c1618d5824e11ddd99e10569191e5708
e-d-o-c <79098019+e-d-o-c@users.noreply.github.com>
2023-06-07 16:09:18 +0100

    Output data (#6)
    * rs output values
    
    * fix var

--- environments/development/applications/variables.tf
+++ environments/development/applications/variables.tf
@@ -9,3 +9,10 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "aws_account_id" {
+  description = "Development Account ID."
+  type        = string
+  default     = "844815912454"
+}
+
