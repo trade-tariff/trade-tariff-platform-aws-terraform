4b956ee79189d651ad2004a84890af6f3be8cb7c
Bun Camara <bun.camara@transformuk.com>
2023-06-14 11:01:54 +0100

    remove unused code

--- environments/development/common/varables.tf
+++ environments/development/common/varables.tf
@@ -9,14 +9,3 @@ variable "cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
 }
-
-# variable "region" {
-#   type    = string
-#   default = "eu-west-2"
-# }
-
-# variable "aws_account_id" {
-#   description = "Development Account ID."
-#   type        = string
-#   default     = "844815912454"
-# }
