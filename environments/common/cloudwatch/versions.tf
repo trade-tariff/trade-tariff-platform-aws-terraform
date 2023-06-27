07bb3e42cffed72874c641e9bcdd6877f8407da9
Amber Cronin <amber@amber.vision>
2023-06-27 14:59:00 +0100

    FIX: Build KMS key policy using interpolation

--- environments/common/cloudwatch/versions.tf
+++ environments/common/cloudwatch/versions.tf
@@ -8,6 +8,4 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+provider "aws" {}
--- environments/development/common/cloudwatch.tf
+++ environments/development/common/cloudwatch.tf
@@ -2,5 +2,4 @@ module "cloudwatch" {
   source            = "../../common/cloudwatch/"
   name              = "platform-logs-${var.environment}"
   retention_in_days = 30
-  region            = var.region
 }
