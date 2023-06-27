e0a3a71bc99d0a8b0fee5d4649c9bbf6cd83cee0
Amber Cronin <amber@amber.vision>
2023-06-27 11:56:51 +0100

    BAU: Fix retention time being unexpected input

--- environments/development/common/cloudwatch.tf
+++ environments/development/common/cloudwatch.tf
@@ -1,6 +1,6 @@
 module "cloudwatch" {
   source            = "../../common/cloudwatch/"
   name              = "platform-logs-${var.environment}"
-  retention_in_days = 31
+  retention_in_days = 30
   region            = var.region
 }
