01c9dd268dfe0ed859d5174c64a8877442d94900
Amber Cronin <amber@amber.vision>
2023-06-30 09:08:50 +0100

    BAU: Replace hyphens with underscores in SSM
    To ensure consistency, I've replaced all hyphens with underscores in
    the SSM parameter names.

--- environments/development/common/ecr.tf
+++ environments/development/common/ecr.tf
@@ -6,7 +6,7 @@ module "ecr" {
 
 resource "aws_ssm_parameter" "ecr_url" {
   for_each    = module.ecr.repository_urls
-  name        = "/${var.environment}/${upper(each.key)}_ECR_URL"
+  name        = "/${var.environment}/${replace(upper(each.key), "-", "_")}_ECR_URL"
   description = "${title(each.key)} ECR repository URL for ${var.environment}."
   type        = "SecureString"
   value       = each.value
