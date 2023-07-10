bd73ab2bddc6f6f53945b6beeeb9d99f40fa9d52
Amber Cronin <amber@amber.vision>
2023-07-10 10:21:20 +0100

    BAU: Allow HTTP traffic through to ECS from ALB
    [ci skip] because I don't want this to apply over the other open PRs
    
    We want HTTP traffic to be reach the applications from the load
    balancer, because the applications health checks are done by the load
    balancer - and can only be done over a non-SSL connection. This is the
    simplest way of digging ourselves out of that hole.
    
    I've also removed variables that were only being used once or twice.

--- environments/common/security-group/security-group.tf
+++ environments/common/security-group/security-group.tf
@@ -1,26 +1,27 @@
 # application load balancer Security Group
 # This is to supress tfsec erroring allow all ingress traffic to the load balancer
 
-# tfsec:ignore:aws-ec2-no-public-ingress-sgr
 resource "aws_security_group" "alb_security_group" {
   name        = "trade-tariff-alb-security-group-${var.environment}"
   description = "Allow TLS inbound traffic"
   vpc_id      = data.terraform_remote_state.base.outputs.vpc_id
 
+  # tfsec:ignore:aws-ec2-no-public-ingress-sgr
   ingress {
-    description = "https access"
+    description = "HTTPS Ingress from CDN"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_ingress_traffic]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # tfsec:ignore:aws-ec2-no-public-egress-sgr
   egress {
     description = "Allow All Egress From VPC"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [var.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -31,16 +32,22 @@ resource "aws_security_group" "alb_security_group" {
 /* Security group for the ecs application */
 resource "aws_security_group" "ecs_security_group" {
   name        = "trade-tariff-ecs-security-group-${var.environment}"
-  description = "Allow TLS ingress, all egress"
+  description = "Allow HTTP/S ingress, all egress"
   vpc_id      = data.terraform_remote_state.base.outputs.vpc_id
 
   # ingress traffic from alb
   ingress {
-    description     = "HTTPS Access From ALB"
-    from_port       = 443
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_security_group.id]
+    description = "HTTP Access from ALB"
+    from_port   = 80
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+
+  ingress {
+    description = "HTTPS Access From ALB"
+    from_port   = 443
+    to_port     = 8080
+    protocol    = "tcp"
   }
 
   # We want egress out to the public internet here
