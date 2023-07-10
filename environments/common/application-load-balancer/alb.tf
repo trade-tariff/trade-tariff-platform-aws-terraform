da794a99246fb1782b330affb9be620fe96b3198
Amber Cronin <amber@amber.vision>
2023-07-10 14:45:00 +0100

    BAU: Remove additional cert resource
    We are already supplying the certificate to the listener in its own
    resource, so we don't need this additional resource that does the same
    thing.

--- environments/common/application-load-balancer/alb.tf
+++ environments/common/application-load-balancer/alb.tf
@@ -53,7 +53,6 @@ resource "aws_lb_listener" "trade_tariff_listeners" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.trade_tariff_target_groups["trade-tariff-fe-tg-${var.environment}"].arn
   }
-
 }
 
 resource "aws_lb_listener_rule" "trade_tariff_backend_listeners_rules" {
@@ -87,9 +86,4 @@ resource "aws_lb_listener_rule" "trade_tariff_duty_cal_listeners_rules" {
       values = ["/duty-calulator/*"]
     }
   }
-}
-
-resource "aws_lb_listener_certificate" "https_cert_resource" {
-  listener_arn    = aws_lb_listener.trade_tariff_listeners.arn
-  certificate_arn = var.certificate_arn
 }
