306abe3d00f6c05b647e2ab2ea908ef05fe19105
Amber Cronin <amber@amber.vision>
2023-08-03 17:27:05 +0100

    BAU: Set up healthcheck path dynamically [ci skip]

--- environments/common/application-load-balancer/alb.tf
+++ environments/common/application-load-balancer/alb.tf
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "trade_tariff_target_groups" {
   health_check {
     enabled             = true
     interval            = 60
-    path                = "/healthcheckz"
+    path                = each.value.healthcheck_path
     port                = "traffic-port"
     healthy_threshold   = 3
     unhealthy_threshold = 3
