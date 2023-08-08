38137335cae7715198d10118d581eda39032b6de
Purushothama Venkat <venkatp@MacBook-Pro.local>
2023-08-08 11:56:12 +0100

    HOTT-3747 - replacing old redis v5 cluster with new multi-node redis v7.x cluster

--- environments/development/common/redis.tf
+++ environments/development/common/redis.tf
@@ -25,41 +25,6 @@ resource "aws_cloudwatch_log_group" "redis_engine_lg" {
   retention_in_days = 30
 }
 
-### Single node redis cluster - v1 deployment (resulted in connectivity issues due to endpoints being blank)
-# module "redis" {
-#   source   = "../../common/elasticache/"
-#   for_each = local.redis
-
-#   group_name           = "redis-${each.key}-${var.environment}"
-#   redis_version        = "5.0.6"
-#   instance_type        = "cache.t3.micro"
-#   parameter_group      = "default.redis5.0"
-#   cloudwatch_log_group = module.cloudwatch.log_group_name
-
-#   security_group_ids = [module.alb-security-group.redis_security_group_id]
-#   subnet_group_name  = aws_elasticache_subnet_group.this.name
-
-#   maintenance_window = "sun:04:00-sun:05:00"
-#   snapshot_window    = "02:00-04:00"
-# }
-
-# resource "aws_elasticache_subnet_group" "this" {
-#   name       = "redis-subnet-group"
-#   subnet_ids = data.terraform_remote_state.base.outputs.private_subnet_ids
-# }
-
-# resource "aws_secretsmanager_secret" "redis_connection_string" {
-#   for_each   = local.redis
-#   name       = "redis-${each.key}-connection-string"
-#   kms_key_id = aws_kms_key.secretsmanager_kms_key.arn
-# }
-
-# resource "aws_secretsmanager_secret_version" "redis_connection_string_value" {
-#   for_each      = local.redis
-#   secret_id     = aws_secretsmanager_secret.redis_connection_string[each.key].id
-#   secret_string = "redis://${module.redis[each.key].endpoint}:6379"
-# }
-
 ### Multi node redis with cluster mode disabled (one primary and two replica nodes spread across all 3 availability zones)
 module "redis" {
   source   = "../../common/elasticache-redis/"
@@ -77,7 +42,7 @@ module "redis" {
   automatic_failover_enabled  = true
   preferred_cache_cluster_azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 
-  ## To take up enabling redis encryption after migration
+  # To take up enabling redis encryption after migration
   # at_rest_encryption_enabled = true
   # transit_encryption_enabled = true
 
