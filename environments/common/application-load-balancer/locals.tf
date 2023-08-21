12c18dfe12f2a80bc55aeb36f5e20cf1f18a4b61
Amber Cronin <amber@amber.vision>
2023-08-21 12:24:36 +0100

    HOTT-3876: Create blue/green target groups

--- environments/common/application-load-balancer/locals.tf
+++ environments/common/application-load-balancer/locals.tf
@@ -1,53 +1,67 @@
 locals {
   services = {
     admin = {
-      target_group_name = "trade-tariff-ad-tg-${var.environment}"
-      priority          = 10
-      host              = ["admin.*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 10
+      host             = ["admin.*"]
+      healthcheck_path = "/healthcheckz"
     }
 
     signon = {
-      target_group_name = "trade-tariff-so-tg-${var.environment}"
-      priority          = 20
-      host              = ["signon.*"]
-      healthcheck_path  = "/healthcheck/live"
+      priority         = 20
+      host             = ["signon.*"]
+      healthcheck_path = "/healthcheck/live"
     }
 
     backend_uk = {
-      target_group_name = "backend-uk-tg-${var.environment}"
-      priority          = 30
-      paths             = ["/uk/api/beta/*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 30
+      paths            = ["/uk/api/beta/*"]
+      healthcheck_path = "/healthcheckz"
     }
 
     backend_xi = {
-      target_group_name = "backend-xi-tg-${var.environment}"
-      priority          = 35
-      paths             = ["/xi/api/beta/*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 35
+      paths            = ["/xi/api/beta/*"]
+      healthcheck_path = "/healthcheckz"
     }
 
     duty_calculator = {
-      target_group_name = "trade-tariff-dc-tg-${var.environment}"
-      priority          = 40
-      paths             = ["/duty-calculator/*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 40
+      paths            = ["/duty-calculator/*"]
+      healthcheck_path = "/healthcheckz"
     }
 
     search_query_parser = {
-      target_group_name = "trade-tariff-sqp-tg-${var.environment}"
-      priority          = 50
-      paths             = ["/api/search/*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 50
+      paths            = ["/api/search/*"]
+      healthcheck_path = "/healthcheckz"
     }
 
     # frontend is a fallback, so must have the highest priority
     frontend = {
-      target_group_name = "trade-tariff-fe-tg-${var.environment}"
-      priority          = 100
-      paths             = ["/*"]
-      healthcheck_path  = "/healthcheckz"
+      priority         = 100
+      paths            = ["/*"]
+      healthcheck_path = "/healthcheckz"
     }
   }
+
+
+  blue = {
+    for k, v in local.services : "${k}_blue" => {
+      priority         = v.priority + 5
+      host             = try(v.host, null)
+      paths            = try(v.paths, null)
+      healthcheck_path = v.healthcheck_path
+    }
+  }
+
+  green = {
+    for k, v in local.services : "${k}_green" => {
+      priority         = v.priority + 5
+      host             = try(v.host, null)
+      paths            = try(v.paths, null)
+      healthcheck_path = v.healthcheck_path
+    }
+  }
+
+  blue_green = merge(local.blue, local.green)
 }
