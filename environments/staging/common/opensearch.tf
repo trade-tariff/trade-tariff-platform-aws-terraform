a8cabce419c3d4ca292a40f2da5ba0c843251922
Amber Cronin <amber@amber.vision>
2023-08-23 15:27:37 +0100

    HOTT-3855: Increase instance sizes for staging

--- environments/staging/common/opensearch.tf
+++ environments/staging/common/opensearch.tf
@@ -78,7 +78,7 @@ module "opensearch" {
   master_instance_enabled = false
   warm_instance_enabled   = false
   instance_count          = 3
-  instance_type           = "t3.small.search" # small one for dev :)
+  instance_type           = "m6.xlarge.search"
   ebs_volume_size         = 80
 
   create_master_user = true
--- environments/staging/common/rds.tf
+++ environments/staging/common/rds.tf
@@ -6,10 +6,9 @@ module "postgres" {
   engine         = "postgres"
   engine_version = "13.11"
 
-  deletion_protection = false # while configuring
+  deletion_protection = true
 
-  # smallest that supports encryption at rest and postgres 13.11
-  instance_type      = "db.t3.micro"
+  instance_type      = "db.t3.large"
   backup_window      = "22:00-23:00"
   maintenance_window = "Fri:23:00-Sat:01:00"
   private_subnet_ids = data.terraform_remote_state.base.outputs.private_subnet_ids
@@ -33,7 +32,7 @@ module "mysql" {
   engine         = "mysql"
   engine_version = "8.0"
 
-  deletion_protection = false # in dev
+  deletion_protection = true
 
   instance_type      = "db.t3.medium"
   backup_window      = "22:00-23:00"
