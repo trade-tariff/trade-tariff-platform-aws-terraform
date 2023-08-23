4f59ddba2d89d47ebd4a52960a710d7dd9d6844c
Amber Cronin <amber@amber.vision>
2023-08-23 15:51:32 +0100

    BAU: Fix security groups module

--- environments/common/security-group/variables.tf
+++ environments/common/security-group/variables.tf
@@ -3,19 +3,12 @@ variable "environment" {
   type        = string
 }
 
-variable "region" {
-  description = "AWS region. Defaults to `eu-west-2`."
-  type        = string
-  default     = "eu-west-2"
-}
-
-variable "aws_account_id" {
-  description = "Development Account ID."
-  type        = string
-  default     = "844815912454"
-}
-
 variable "private_subnets" {
   description = "A list of private subnets inside the VPC"
   type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC."
+  type        = string
 }
--- environments/development/common/security-group.tf
+++ environments/development/common/security-group.tf
@@ -2,4 +2,5 @@ module "alb-security-group" {
   source          = "../../common/security-group/"
   environment     = var.environment
   private_subnets = data.terraform_remote_state.base.outputs.private_subnets_cidr_blocks
+  vpc_id          = data.terraform_remote_state.base.outputs.vpc_id
 }
