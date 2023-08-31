59e78556a73dd694c61d347a2cdaf5428dcbd00c
Amber Cronin <amber@amber.vision>
2023-08-31 14:57:12 +0100

    BAU: Remove redundant Signon resources

--- environments/production/common/locals.tf
+++ environments/production/common/locals.tf
@@ -2,14 +2,6 @@ locals {
   account_id         = data.aws_caller_identity.current.account_id
   origin_domain_name = "origin.${var.domain_name}"
 
-  applications = [
-    "admin",
-    "backend",
-    "duty-calculator",
-    "frontend",
-    "search-query-parser",
-    "signon"
-  ]
 }
 
 data "aws_caller_identity" "current" {}
