4892a228eb76ff845e1f547277d2cd38c6a8171a
Amber Cronin <amber@amber.vision>
2023-09-07 16:35:27 +0100

    BAU: Add Signon Postgres

--- environments/development/common/rds.tf
+++ environments/development/common/rds.tf
@@ -1,3 +1,4 @@
+# Backend Postgres
 module "postgres" {
   source = "../../common/rds"
 
@@ -25,6 +26,35 @@ module "postgres" {
   ]
 }
 
+# Admin Postgres
+module "postgres_admin" {
+  source = "../../common/rds"
+
+  environment    = var.environment
+  name           = "PostgresAdmin"
+  engine         = "postgres"
+  engine_version = "13.11"
+
+  deletion_protection = false # while configuring
+
+  # smallest that supports encryption at rest and postgres 13.11
+  instance_type      = "db.t3.micro"
+  backup_window      = "22:00-23:00"
+  maintenance_window = "Fri:23:00-Sat:01:00"
+  private_subnet_ids = data.terraform_remote_state.base.outputs.private_subnet_ids
+
+  allocated_storage     = 20
+  max_allocated_storage = 40
+  security_group_ids    = [module.alb-security-group.be_to_rds_security_group_id]
+
+  secret_kms_key_arn = aws_kms_key.secretsmanager_kms_key.arn
+
+  depends_on = [
+    module.alb-security-group
+  ]
+}
+
+# Signon MySQL
 module "mysql" {
   source = "../../common/rds"
 
