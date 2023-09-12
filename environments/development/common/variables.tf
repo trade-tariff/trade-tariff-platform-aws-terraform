c7ac00b4bca12ebe78e98c9867bc9b31a6795d04
Amber Cronin <amber@amber.vision>
2023-09-12 13:16:38 +0100

    BAU: Add key derivation salt secret for Signon

--- environments/development/common/variables.tf
+++ environments/development/common/variables.tf
@@ -171,3 +171,9 @@ variable "signon_govuk_notify_api_key" {
   type        = string
   sensitive   = true
 }
+
+variable "signon_derivation_salt" {
+  description = "Value of ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT for the signon app."
+  type        = string
+  sensitive   = true
+}
