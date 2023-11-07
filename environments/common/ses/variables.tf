49c0a92493759fb9e6fadc3bf05de30a9213abfb
Amber Cronin <amber@amber.vision>
2023-11-07 16:42:25 +0000

    HOTT-4310: SES Verified Identity

---
+++ environments/common/ses/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  description = "Domain name to verify."
+  type        = string
+}
+
+variable "route53_zone_id" {
+  description = "Route 53 hosted zone ID."
+  type        = string
+}
