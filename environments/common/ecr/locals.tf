5da7ad107bc0b2e941ad1419bfe6335ff1878a2d
Amber Cronin <amber@amber.vision>
2023-11-15 10:12:51 +0000

    BAU: Add ECR Repo for Database Backups
    Sort applications list

--- environments/common/ecr/locals.tf
+++ environments/common/ecr/locals.tf
@@ -2,10 +2,11 @@ locals {
   applications = [
     "admin",
     "backend",
+    "database-backups",
     "duty-calculator",
+    "fpo-search",
     "frontend",
     "search-query-parser",
     "signon",
-    "fpo-search"
   ]
 }
