2ab1e1538c302c453cfdf5d80fa9e758101b975a
Amber Cronin <amber@amber.vision>
2024-01-08 15:51:22 +0000

    HOTT-4729: Update CDN, ALB to restrict origin access

--- environments/production/common/alb.tf
+++ environments/production/common/alb.tf
@@ -2,7 +2,12 @@ module "alb" {
   source                = "../../common/application-load-balancer/"
   alb_name              = "trade-tariff-alb-${var.environment}"
   alb_security_group_id = module.alb-security-group.alb_security_group_id
-  public_subnet_ids     = data.terraform_remote_state.base.outputs.public_subnet_ids
   certificate_arn       = module.acm_origin.validated_certificate_arn
+  public_subnet_ids     = data.terraform_remote_state.base.outputs.public_subnet_ids
   vpc_id                = data.terraform_remote_state.base.outputs.vpc_id
+
+  custom_header = {
+    name  = random_password.origin_header[0].result
+    value = random_password.origin_header[1].result
+  }
 }
