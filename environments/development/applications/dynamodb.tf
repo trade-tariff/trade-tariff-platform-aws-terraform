620c4bafad62ca35b26bb730bed425d5ac600a25
William Fish <william.michael.fish@gmail.com>
2024-03-20 10:42:22 +0000

    FPO-132: Adds dynamo db table for CustomerApiKeys in development

--- environments/development/applications/dynamodb.tf
+++ environments/development/applications/dynamodb.tf
@@ -10,3 +10,45 @@ resource "aws_dynamodb_table" "lock" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "customer_api_keys" {
+  name         = "CustomerApiKeys"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "CustomerApiKeyId" # Unique identifier for the API key
+  range_key    = "CustomerId"       # Localized in dynamodb for each SCP customer that logs in
+
+  attribute {
+    name = "CustomerApiKeyId"
+    type = "S"
+  }
+
+  attribute {
+    name = "CustomerId"
+    type = "S"
+  }
+
+  attribute {
+    name = "Secret"
+    type = "S"
+  }
+
+  attribute {
+    name = "Description"
+    type = "S"
+  }
+
+  attribute {
+    name = "Enabled"
+    type = "BOOL"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "S"
+  }
+
+  attribute {
+    name = "UpdatedAt"
+    type = "S"
+  }
+}
