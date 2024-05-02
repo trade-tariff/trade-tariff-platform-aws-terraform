b448588a56b87a53bcfde46d1515a214e1b4cf19
William Fish <william.michael.fish@gmail.com>
2024-05-02 16:58:33 +0100

    FPO-201: Fixes security group name

--- environments/development/extra-regions/us-east-1/security_groups.tf
+++ environments/development/extra-regions/us-east-1/security_groups.tf
@@ -1,11 +1,11 @@
-resource "aws_security_group" "this" {
+resource "aws_security_group" "ssh" {
   name        = "allow-ssh"
   description = "Allow inbound SSH traffic"
   vpc_id      = module.vpc.vpc_id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.this.id
+  security_group_id = aws_security_group.ssh.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
   from_port         = 22
@@ -13,7 +13,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
-  security_group_id = aws_security_group.this.id
+  security_group_id = aws_security_group.ssh.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
