c094944c98c2b839fba1d3d71b226445e02c5ffb
Amber Cronin <amber@amber.vision>
2025-02-03 11:54:18 +0000

    HMRC-532: Set engine correctly in output strings

--- modules/rds_cluster/locals.tf
+++ modules/rds_cluster/locals.tf
@@ -1,6 +1,12 @@
 locals {
-  engine  = var.engine == "mysql" ? "mysql2" : var.engine
-  options = var.engine == "mysql" ? "?reconnect=true&useSSL=true" : ""
+  engines = {
+    "aurora-postgresql" = "postgres"
+    "aurora-mysql"      = "mysql"
+  }
+  engine = local.engines[var.engine]
+  options = (
+    var.engine == "mysql" ? "?reconnect=true&useSSL=true" : ""
+  )
   rw_string = (
     "${local.engine}://${var.username}:${random_password.master_password.result}@${aws_rds_cluster.this.endpoint}/${aws_rds_cluster.this.database_name}${local.options}"
   )
