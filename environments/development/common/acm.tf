7b125feb2cd804e8ec82c9a07c230813d5a0d81c
William Fish <william.michael.fish@gmail.com>
2025-02-12 12:36:08 +0000

    HMRC-657: Remove old preview app infrastructure

--- environments/development/common/acm.tf
+++ environments/development/common/acm.tf
@@ -10,17 +10,6 @@ module "acm" {
   }
 }
 
-module "acm_preview" {
-  source         = "../../../modules/acm"
-  domain_name    = "preview.${var.domain_name}"
-  environment    = var.environment
-  hosted_zone_id = data.aws_route53_zone.this.zone_id
-
-  providers = {
-    aws = aws.us_east_1
-  }
-}
-
 module "acm_london" {
   source         = "../../../modules/acm/"
   domain_name    = var.domain_name
