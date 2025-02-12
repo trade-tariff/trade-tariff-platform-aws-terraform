3e4182039e6efff1711d40cbfbbdbc251baa316b
William Fish <william.michael.fish@gmail.com>
2025-02-12 16:54:02 +0000

    BAU: Cut a new WAF release

--- environments/staging/common/waf.tf
+++ environments/staging/common/waf.tf
@@ -1,5 +1,5 @@
 module "waf" {
-  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/waf?ref=aws/waf-v1.2.3"
+  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/waf?ref=aws/waf-v1.2.4"
 
   name  = "tariff-waf-${var.environment}"
   scope = "CLOUDFRONT"
