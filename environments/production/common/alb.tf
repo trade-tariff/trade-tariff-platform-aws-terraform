8542a44c87ff863d22d448f4e17c55248cf9d74e
William Fish <william.michael.fish@gmail.com>
2025-02-12 14:08:59 +0000

    HMRC-657: Make sure production paths are correct

--- environments/production/common/alb.tf
+++ environments/production/common/alb.tf
@@ -51,13 +51,13 @@ module "alb" {
     }
 
     # backend_uk = {
-    #   paths            = ["/api", "/uk/api"]
+    #   paths            = ["/api/*", "/uk/api/*"]
     #   healthcheck_path = "/healthcheckz"
     #   priority         = 20
     # }
     #
     # backend_xi = {
-    #   paths            = ["/xi/api"]
+    #   paths            = ["/xi/api/*"]
     #   healthcheck_path = "/healthcheckz"
     #   priority         = 21
     # }
