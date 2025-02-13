895779f3c1f529eb1881cd965b8f83451a617591
William Fish <william.michael.fish@gmail.com>
2025-02-13 15:30:26 +0000

    BAU: Debug 500s

--- environments/production/common/alb.tf
+++ environments/production/common/alb.tf
@@ -51,16 +51,17 @@ module "alb" {
     }
 
     backend_uk = {
-      paths            = ["/api/*", "/uk/api/*"]
+      paths            = ["/new/api/*", "/new/uk/api/*"]
       healthcheck_path = "/healthcheckz"
       priority         = 20
     }
 
     backend_xi = {
-      paths            = ["/xi/api/*"]
+      paths            = ["/new/xi/api/*"]
       healthcheck_path = "/healthcheckz"
       priority         = 21
     }
+
 
     frontend = {
       paths            = ["/*"]
