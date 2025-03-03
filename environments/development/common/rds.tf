c97a207b13e810960cba9114987de2bc133a9f86
Amber Cronin <amber@amber.vision>
2025-03-03 16:04:59 +0000

    BAU: Add dev aurora cluster
    It was missing

--- environments/development/common/rds.tf
+++ environments/development/common/rds.tf
@@ -96,6 +96,40 @@ module "postgres_commodi_tea" {
   }
 }
 
+# Aurora cluster
+module "postgres_aurora" {
+  source = "../../../modules/rds_cluster"
+
+  cluster_name      = "postgres-aurora-${var.environment}"
+  engine            = "aurora-postgresql"
+  engine_version    = "13.15"
+  engine_mode       = "provisioned"
+  cluster_instances = 2
+  apply_immediately = true
+
+  instance_class = "db.serverless"
+  database_name  = "TradeTariffPostgres${title(var.environment)}"
+  username       = "tariff"
+
+  min_capacity = 0.5
+  max_capacity = 256
+
+  security_group_ids = [module.alb-security-group.be_to_rds_security_group_id]
+  private_subnet_ids = data.terraform_remote_state.base.outputs.private_subnet_ids
+
+  tags = {
+    "RDS_Type" = "Aurora"
+  }
+}
+
+module "rw_aurora_connection_string" {
+  source          = "../../../modules/secret/"
+  name            = "aurora-postgres-rw-connection-string"
+  kms_key_arn     = aws_kms_key.secretsmanager_kms_key.arn
+  recovery_window = 7
+  secret_string   = module.postgres_aurora.rw_connection_string
+}
+
 module "postgres_admin_aurora" {
   source = "../../../modules/rds_cluster"
 
