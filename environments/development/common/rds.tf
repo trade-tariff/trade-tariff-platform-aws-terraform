3d8b10791174eeb89f9c4975b10f06d2c0a1162c
Amber Cronin <amber@amber.vision>
2025-03-10 10:35:15 +0000

    HMRC-725: Upgrade to Postgres16

--- environments/development/common/rds.tf
+++ environments/development/common/rds.tf
@@ -1,34 +1,3 @@
-# Backend Postgres
-module "postgres" {
-  source = "../../../modules/rds"
-
-  environment    = var.environment
-  name           = "TradeTariffPostgres${title(var.environment)}"
-  engine         = "postgres"
-  engine_version = "13.15"
-
-  deletion_protection = false # while configuring
-
-  instance_type      = "db.t3.medium"
-  backup_window      = "22:00-23:00"
-  maintenance_window = "Fri:23:00-Sat:01:00"
-  private_subnet_ids = data.terraform_remote_state.base.outputs.private_subnet_ids
-
-  allocated_storage     = 20
-  max_allocated_storage = 40
-  security_group_ids    = [module.alb-security-group.be_to_rds_security_group_id]
-
-  secret_kms_key_arn = aws_kms_key.secretsmanager_kms_key.arn
-
-  depends_on = [
-    module.alb-security-group
-  ]
-
-  tags = {
-    Name = "TradeTariffPostgres${title(var.environment)}"
-  }
-}
-
 # Signon MySQL
 module "mysql" {
   source = "../../../modules/rds"
@@ -62,7 +31,7 @@ module "postgres_commodi_tea" {
   environment    = var.environment
   name           = "PostgresCommodiTea"
   engine         = "postgres"
-  engine_version = "16.3"
+  engine_version = "16.8"
 
   deletion_protection = false
   multi_az            = false
@@ -94,7 +63,7 @@ module "postgres_aurora" {
 
   cluster_name      = "postgres-aurora-${var.environment}"
   engine            = "aurora-postgresql"
-  engine_version    = "13.15"
+  engine_version    = "16.8"
   engine_mode       = "provisioned"
   cluster_instances = 2
   apply_immediately = true
@@ -127,7 +96,7 @@ module "postgres_admin_aurora" {
 
   cluster_name      = "admin-aurora-${var.environment}"
   engine            = "aurora-postgresql"
-  engine_version    = "13.15"
+  engine_version    = "16.8"
   engine_mode       = "provisioned"
   cluster_instances = 1
   apply_immediately = true
