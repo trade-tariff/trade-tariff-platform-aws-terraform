b5f0dec1f937696114419c7b8b8b09338084d29e
Aigul Djon <aigul.djon@gmail.com>
2025-03-14 15:46:42 +0000

    Add application-autoscaling permissions for GitHub Actions setup

--- environments/staging/common/iam-policies.tf
+++ environments/staging/common/iam-policies.tf
@@ -9,6 +9,7 @@ resource "aws_iam_policy" "ci_terraform_policy" {
         Effect = "Allow",
         Action = [
           "acm:*",
+          "application-autoscaling:*",
           "autoscaling:*",
           "cloudformation:*",
           "cloudfront:*",
