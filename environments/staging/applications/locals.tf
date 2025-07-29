68724cd24ff1b6697a78d9054fa68993e42695f8
Aigul Djon <aigul.djon@gmail.com>
2025-07-29 09:59:15 +0100

    Delete dynamodb locals

--- environments/staging/applications/locals.tf
+++ environments/staging/applications/locals.tf
@@ -1,11 +1,3 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  applications = [
-    "admin",
-    "backend",
-    "dev-hub",
-    "frontend",
-    "identity",
-    "tea"
-  ]
 }
