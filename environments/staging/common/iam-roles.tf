c07042959163ed96faa4224710810d5ec93e04cb
Aigul Djon <aigul.djon@gmail.com>
2025-08-08 15:15:46 +0100

    Add admin repo to the e2e test role

--- environments/staging/common/iam-roles.tf
+++ environments/staging/common/iam-roles.tf
@@ -288,6 +288,7 @@ resource "aws_iam_role" "e2e_testing_ci_role" {
               "repo:trade-tariff/trade-tariff-frontend:*",
               "repo:trade-tariff/trade-tariff-platform-aws-terraform:*",
               "repo:trade-tariff/trade-tariff-commodi-tea:*",
+              "repo:trade-tariff/trade-tariff-admin:*",
             ]
           }
         }
