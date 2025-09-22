4366acfcf5b4d13ffdecbdefdcbbd52949d7bb2f
William Fish <william.michael.fish@gmail.com>
2025-09-22 10:55:23 +0100

    HMRC-1604: Updates all dependencies
    Unblocks compatibility with newer formats of lockfile

--- environments/production/common/versions.tf
+++ environments/production/common/versions.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 1.12"
+  required_version = ">= 1.12.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6"
+      version = ">= 6.0.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3"
+      version = ">= 3.0.0"
     }
 
     archive = {
       source  = "hashicorp/archive"
-      version = ">= 2"
+      version = ">= 2.0.0"
     }
   }
 }
--- modules/cognito/versions.tf
+++ modules/cognito/versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.12"
+  required_version = ">= 1.12.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6"
+      version = ">= 6.0.0"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = ">= 3"
+      version = ">= 3.0.0"
     }
   }
 }
