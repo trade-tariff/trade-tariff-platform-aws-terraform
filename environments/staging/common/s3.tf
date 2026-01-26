ce79a9405b731a61c5a8bef53b430be2fe03b919
Aigul Djon <aigul.djon@gmail.com>
2026-01-26 15:41:06 +0000

    Add NewRelic to staging

--- environments/staging/common/s3.tf
+++ environments/staging/common/s3.tf
@@ -7,6 +7,7 @@ locals {
     persistence       = "trade-tariff-persistence-${local.account_id}"
     reporting         = "trade-tariff-reporting-${local.account_id}"
     ses-inbound       = "trade-tariff-ses-inbound-${local.account_id}"
+    firehose_backups  = "trade-tariff-firehose-backups-${local.account_id}"
   }
   buckets_with_versioning = {
     persistence = local.buckets.persistence
@@ -196,6 +197,21 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
 
     filter {
       prefix = each.value.prefix
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "firehose_backups_rotation" {
+  bucket = aws_s3_bucket.this["firehose_backups"].id
+
+  rule {
+    id     = "rotate-firehose-backups"
+    status = "Enabled"
+
+    filter {}
+
+    expiration {
+      days = 15
     }
   }
 }
