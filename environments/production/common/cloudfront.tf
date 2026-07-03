00a09d349b5ea292872090926a83b274b9259206
William Fish <william.michael.fish@gmail.com>
2026-07-03 15:34:43 +0100

    fix: extend production CDN timeout
    Issue: No ticket/issue

--- environments/production/common/cloudfront.tf
+++ environments/production/common/cloudfront.tf
@@ -32,6 +32,7 @@ module "cdn" {
         https_port             = 443
         origin_protocol_policy = "https-only"
         origin_ssl_protocols   = ["TLSv1.2"]
+        origin_read_timeout    = 100
       }
 
       custom_header = [{
